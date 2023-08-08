8c9b16b74cb8b1eec60c8670c648e50ddfafade3
David Dolheguy <ddolheguy@gmail.com>
2023-08-08 21:15:04 +0100

    Small updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import ical from 'ical-generator';
+import ical, { ICalCalendarMethod } from 'ical-generator';
 import express from 'express';
+import { DateTime } from 'luxon';
 
 const PORT = process.env.PORT || 5001
 
@@ -7,21 +8,27 @@ const app = express();
 app.use(express.json());
 
 app.get('/feed.ical', (req, res) => {
-    const calendar = ical({name: 'my first iCal'});
+    const updated = DateTime.utc().toISO();
+    const calendar = ical({name: `my first - ${updated}`});
 
+    calendar.source('https://ical-test-9982b15a03ae.herokuapp.com/feed.ical');
     calendar.url('https://ical-test-9982b15a03ae.herokuapp.com/feed.ical');
-    calendar.ttl(60); // 10 seconds
+    calendar.ttl(30); // 30 seconds
 
-    const startTime = new Date();
-    const endTime = new Date();
+    const startTime = DateTime.utc().plus({ days: 1 })
+    const endTime = startTime.plus({ hours: 4 });
 
     endTime.setHours(startTime.getHours()+1);
 
+    calendar.method(ICalCalendarMethod.PUBLISH);
     calendar.createEvent({
+        id: '20030808T12100000Z-12927wd7@example.com',
+        // sequence: Math.ceil(DateTime.utc().toSeconds() / 60),
+        timezone: 'Europe/London',
         start: startTime,
         end: endTime,
         summary: 'Test Event',
-        description: 'This is a description',
+        description: `This is a description - updated: ${updated} `,
         location: 'Some location',
         url: 'https://test.com/'
     });
